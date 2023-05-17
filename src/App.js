b1840cbc4c0200f68d030ff4316927736c2f4fd9
Meenakshi-Sivakumar <meenakshisivakumar08@gmail.com>
2023-05-17 11:28:36 +0530

    all done

--- src/App.js
+++ src/App.js
@@ -2,7 +2,22 @@ import React, { useState } from 'react';
 import data from './data';
 import SingleQuestion from './Question';
 function App() {
-  return <h2>accordion project setup</h2>;
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = (id, open) => {
+    setIsOpen(!isOpen);
+    data[id-1].open = isOpen;
+  }
+
+  return (
+    <main>
+    <section className='section'>
+    <div className='container'>
+    {data.map(data=> <SingleQuestion key={data.id} {...data} toggle={toggle} isOpen={isOpen}/> )}
+    </div>
+    </section>
+    </main>
+  );
 }
 
 export default App;
