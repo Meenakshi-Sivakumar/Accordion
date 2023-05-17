2a51eca7f502d208808937212efe865f48654a7a
Meenakshi-Sivakumar <meenakshisivakumar08@gmail.com>
2023-05-17 13:02:24 +0530

    all done

--- src/Question.js
+++ src/Question.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
 const Question = ({id, title, info, open,toggle}) => {
   return (
-    <div>
+    <div className='accordion'>
       <div className='accordion-header' onClick={() => toggle(id, open)}>
-        <h4>{title}</h4>
-        <span>{open ? <AiOutlineMinus /> : <AiOutlinePlus />}</span>
+        <h4 className={open? 'dark': 'light'}>{title}</h4>
+        <span>{open ? <button className='btn open'><AiOutlineMinus /> </button>: <button className='btn close'><AiOutlinePlus /></button>}</span>
       </div>
-      {open? info : ''}
+      {open? <p>{info}</p> : ''}
+      {id !== 5? <div className='underline'></div> : ''}
     </div>
   );
 };
