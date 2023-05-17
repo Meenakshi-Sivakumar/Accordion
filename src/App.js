52c3ecef6c9b49579158ab02fc53bf3670b6b6b3
Meenakshi-Sivakumar <meenakshisivakumar08@gmail.com>
2023-05-17 13:02:17 +0530

    all done

--- src/App.js
+++ src/App.js
@@ -12,9 +12,17 @@ function App() {
   return (
     <main>
     <section className='section'>
+    <div className='circle one'></div>
+    <div className='circle two'></div>
+    <div className='head'>
+      <h3>The Internet Questions</h3>
+      <p>The following the some popular internet questions displayed in an accordion.</p>
+    </div>
     <div className='container'>
     {data.map(data=> <SingleQuestion key={data.id} {...data} toggle={toggle} isOpen={isOpen}/> )}
     </div>
+    <div className='circle three'></div>
+    <div className='circle four'></div>
     </section>
     </main>
   );
