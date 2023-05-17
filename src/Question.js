da8a4decf260b96b75ac0c3f80627b2677f068b3
Meenakshi-Sivakumar <meenakshisivakumar08@gmail.com>
2023-05-17 11:28:49 +0530

    all done

--- src/Question.js
+++ src/Question.js
@@ -1,7 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+const Question = ({id, title, info, open,toggle}) => {
+  return (
+    <div>
+      <div className='accordion-header' onClick={() => toggle(id, open)}>
+        <h4>{title}</h4>
+        <span>{open ? <AiOutlineMinus /> : <AiOutlinePlus />}</span>
+      </div>
+      {open? info : ''}
+    </div>
+  );
 };
 
 export default Question;
